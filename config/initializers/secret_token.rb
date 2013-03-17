40ed6a9334f68c3137856c30e8a9a6f013afde79
mspiegs <matt@mycaseinc.com>
2013-03-16 21:35:50 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thankly::Application.config.secret_token = '1f5b868979661992a26b3a1db3703ddb15352ecfbc32823022ced43d3d447fe24f74caaad372c692869f42836229c1053c548186c169b07a64dd6a362f5a15f5'
